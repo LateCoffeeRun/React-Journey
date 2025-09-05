437d04bec99d6af74ca5a9c0ecac7085af140884
LateCoffeeRun <96688304+LateCoffeeRun@users.noreply.github.com>
2025-09-05 14:56:19 +0300

    Spa setup (#2)

---
+++ src/pages/CustomHooks.jsx
@@ -0,0 +1,11 @@
+import RamenuRestourantu from "../components/RamenuRestourantu.jsx";
+import TurboTimeoutuuu from "../components/TurboTimeoutuuu.jsx";
+
+export default function CustomHooks(){
+    return (
+        <div>
+            <RamenuRestourantu />
+            <TurboTimeoutuuu />
+        </div>
+    )
+}
